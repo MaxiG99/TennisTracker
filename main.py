ad441b2c3b227a139e182298f00d61c263c83e94
unknown <maximilian-giebel@gmx.de>
2024-03-29 15:07:56 +0100

    Court Line Detection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils import (read_video,
                    save_video)
 from trackers import PlayerTracker, BallTracker
+from court_line_detector import CourtLineDetector
+
 def main():
     # Read Video
     input_video_path = "input_videos/input_video.mp4"
@@ -18,14 +20,19 @@ def main():
                                                      stub_path='tracker_stubs/ball_detections.pkl'
                                                      )
     
-
+    # Court line Detector 
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
 
     # Draw Output
 
     ## Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
-    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
+    output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
+    ## Draw Court Keypoints 
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
     save_video(output_video_frames, "output_videos/output_video.avi")
 
