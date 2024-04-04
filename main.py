92c470dbd8a32eff12512e8ed0e601e9f374caeb
unknown <maximilian-giebel@gmx.de>
2024-04-04 16:45:31 +0200

    Removing of Tracking Persons other than the 2 Players

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils import (read_video,
                    save_video)
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+import cv2
 
 def main():
     # Read Video
@@ -25,6 +26,9 @@ def main():
     court_line_detector = CourtLineDetector(court_model_path)
     court_keypoints = court_line_detector.predict(video_frames[0])
 
+    # choose players
+    player_detections = player_tracker.choose_and_filter_players(court_keypoints, player_detections)
+
     # Draw Output
 
     ## Draw Player Bounding Boxes
@@ -33,6 +37,10 @@ def main():
 
     ## Draw Court Keypoints 
     output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
+
+    ## Draw frame number on top left corner
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"frame:{i}",(10, 30) ,cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
 
     save_video(output_video_frames, "output_videos/output_video.avi")
 
